5eb731c4a09c4a83050f27ef8e1b51c38cc2d6b4
ChristianAtal <christian_atal@hotmail.com>
2020-08-31 15:32:42 +1000

    Updates

---
+++ precache-manifest.d5976faea62e33224acf7854c3246f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b0c06f84821545d6e8067bf2e33817",
+    "url": "/the-dev-collective/index.html"
+  },
+  {
+    "revision": "ccf8bc0ebd1d8b789d28",
+    "url": "/the-dev-collective/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ede473c3a0e75058f23",
+    "url": "/the-dev-collective/static/js/2.e5cb7352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-dev-collective/static/js/2.e5cb7352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf8bc0ebd1d8b789d28",
+    "url": "/the-dev-collective/static/js/main.b980453d.chunk.js"
+  },
+  {
+    "revision": "65e43ebb5ab490fd6e34",
+    "url": "/the-dev-collective/static/js/runtime-main.25f956ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-dev-collective/static/media/logo.5d5d9eef.svg"
+  }
+]);
